7b23e2926c95c5f9d186e9d730c21e8e6658a197
Victor Tang <vtange@users.noreply.github.com>
2022-10-08 14:43:35 -0700

    resizable book so it doesn't overflow for mobile

--- js/book2.js
+++ js/book2.js
@@ -1,1 +1,12 @@
 Page.init();
+function onResize(){
+    var img = document.querySelector(".bb-item img");
+    var displaySetting = img.parentElement.parentElement.style.display;
+    img.parentElement.parentElement.style.display = "block";
+    document.getElementById("bb-bookblock").style.height = img.offsetHeight + "px";
+    img.parentElement.parentElement.style.display = displaySetting;
+}
+window.addEventListener("resize", function(){
+    onResize();
+});
+onResize();
